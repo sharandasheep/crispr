b6cd6bfab8193d7f1f7b72dc398a33a88b6fc70b
sharandasheep <31903966+sharandasheep@users.noreply.github.com>
2019-03-29 10:53:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("nav").style.width = "100%";
 }
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("nav").style.width = "0";
 }
